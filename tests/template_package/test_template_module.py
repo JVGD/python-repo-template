66c15d29cc78220bc83549219149e5e0ce8661d1
Javier Vargas <j.vargas.g.d@gmail.com>
2025-09-29 16:55:11 +0200

    Added tests

--- tests/template_package/test_template_module.py
+++ tests/template_package/test_template_module.py
@@ -1,12 +1,16 @@
 import pytest
+
 from template_package.template_module import Greeter
 
 
-@pytest.mark.parametrize("name, expected_start", [
-    ("Alice", "Hello, Alice!"),
-    ("Bob", "Hello, Bob!"),
-    ("", "Hello, !"),
-])
+@pytest.mark.parametrize(
+    "name, expected_start",
+    [
+        ("Alice", "Hello, Alice!"),
+        ("Bob", "Hello, Bob!"),
+        ("", "Hello, !"),
+    ],
+)
 def test_greet(name: str, expected_start: str) -> None:
     greeter = Greeter(name)
     greeting = greeter.greet()
